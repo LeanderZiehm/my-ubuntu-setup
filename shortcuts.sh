595e2748f5dc763144e5d2a7388d2adc27cc552a
Leander-Arun Ziehm <117998178+LeanderZiehm@users.noreply.github.com>
2025-04-10 13:49:39 +0200

    updated shortcut

--- shortcuts.sh
+++ shortcuts.sh
@@ -1,2 +1,2 @@
 gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys control-center "['<Control>k']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys control-center "['<Super>k']"
